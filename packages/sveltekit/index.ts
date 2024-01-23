01d5bb6c2dec22b9c447bcdd91577f272a554e60
JonathanRiche <j.riche64@gmail.com>
2024-01-23 13:45:36 -0500

    remove svelte in js

--- packages/sveltekit/index.ts
+++ packages/sveltekit/index.ts
@@ -1,8 +1,8 @@
 import Blink from "./src/blink.js";
-import 'svelte';
-import BlinkComponent from "./src/components/blinkcomponent.svelte";
+//import 'svelte';
+//import BlinkComponent from "./src/components/blinkcomponent.svelte";
 
 const blink = new Blink();
 
-export {blink, BlinkComponent};
+export {blink};
 
