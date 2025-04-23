761ff9e80e68864b71930b9442ca1fdc006c9f8e
JonathanRiche <j.riche64@gmail.com>
2025-04-23 14:12:39 -0400

    Moving over domain to legacy domain

--- packages/sveltekit/src/rest.ts
+++ packages/sveltekit/src/rest.ts
@@ -1,16 +1,17 @@
 /// <reference lib="dom" />
 
-import type { ContentItem, seo_fields, contentItemRequest} from "./types/content.js";
+import type { ContentItem, seo_fields, contentItemRequest } from "./types/content.js";
 
 
 //TODO throw error if api token is an empty string
-async function getContentItems(contentItemReq : contentItemRequest){
-    let response : Response;
-    let blinkUrl = `https://blinkcms.com/api/v1/content-item?api_token=${contentItemReq.api_token}&dataFilter=${contentItemReq.dataFilter}`;
-    
-    if (contentItemReq.item_id)blinkUrl=blinkUrl + `&item_id=${contentItemReq.item_id}`;
-    if(contentItemReq.page_url)blinkUrl=blinkUrl + `&page_url=${contentItemReq.page_url}`;
-    if(contentItemReq.collectionId)blinkUrl=blinkUrl + `&collection_id=${contentItemReq.collectionId}`;
+async function getContentItems(contentItemReq: contentItemRequest) {
+    //OLD DOMAIN : blinkcms.com
+    let response: Response;
+    let blinkUrl = `https://legacy.blinkx.io/api/v1/content-item?api_token=${contentItemReq.api_token}&dataFilter=${contentItemReq.dataFilter}`;
+
+    if (contentItemReq.item_id) blinkUrl = blinkUrl + `&item_id=${contentItemReq.item_id}`;
+    if (contentItemReq.page_url) blinkUrl = blinkUrl + `&page_url=${contentItemReq.page_url}`;
+    if (contentItemReq.collectionId) blinkUrl = blinkUrl + `&collection_id=${contentItemReq.collectionId}`;
 
     response = await fetch(blinkUrl);
 
@@ -18,25 +19,25 @@ async function getContentItems(contentItemReq : contentItemRequest){
 
     //Parse this in system instead
     const seoFields = data.seo_fields as any;
-    if(seoFields){
-        try{
+    if (seoFields) {
+        try {
             data.seo_fields = JSON.parse(seoFields) as seo_fields;
-        }catch(e){
+        } catch (e) {
             //console.warn("Could not parse seo fields");
             data.seo_fields = {
-                siteTitle:"",
-                siteDescription:"",
-                favicon:"",
-                metaImage:"",
-                keywords:""
+                siteTitle: "",
+                siteDescription: "",
+                favicon: "",
+                metaImage: "",
+                keywords: ""
             } as seo_fields;
         }
     }
-    
+
 
     return data;
 
 }
 
 
-export {getContentItems};
+export { getContentItems };
